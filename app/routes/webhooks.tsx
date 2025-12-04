d677d5c908c1772705da15269d45db98a18f505f
matsupiee <btq32jh@icloud.com>
2025-12-04 16:50:20 +0900

    /webhooksエンドポイントで認証をしっかり行うように修正

--- app/routes/webhooks.tsx
+++ app/routes/webhooks.tsx
@@ -1,10 +1,10 @@
+import { authenticate } from "app/servers/shopify.server";
 import type { ActionFunctionArgs } from "react-router";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-  console.log("Received webhook request");
-  console.log("Method:", request.method);
-  console.log("URL:", request.url);
+  // 認証を行う
+  await authenticate.webhook(request);
 
-  // Shopify審査用：とりあえず200を返す
-  return new Response(null, { status: 200 });
+  // 認証が成功したら空のレスポンスを返す
+  return new Response();
 };
